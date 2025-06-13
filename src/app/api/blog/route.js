7cb7cb83446e0ee32cb2b379ce591eef4a637620
firstrep <serlaanvesh@gmail.com>
2025-06-13 17:16:13 +0530

    databaseerrorfix

--- src/app/api/blog/route.js
+++ src/app/api/blog/route.js
@@ -6,10 +6,10 @@ import { NextResponse } from "next/server";
 export const GET = async()=>{
    try{
     console.log("heyy iam in route2") 
-    await connectToDb();
-    const posts= await Post.find();
+    // await connectToDb();
+    // const posts= await Post.find();
     console.log(posts);
-    return NextResponse.json(posts);
+    // return NextResponse.json(posts);
 }catch(error){
     console.log(error);
     throw new Error("Failed to fetch posts!");
