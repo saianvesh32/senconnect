51cec55ed76bf43d277c68fbd909685e3623da45
firstrep <serlaanvesh@gmail.com>
2025-06-13 16:56:22 +0530

    layouterrorfix

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
+import Script from "next/script";
 // import { SessionProvider } from "next-auth/react";  // Import SessionProvider
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,7 +20,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script src="https://cdn.tailwindcss.com"></script>
+        <Script
+          src="https://cdn.tailwindcss.com"
+          strategy="beforeInteractive"
+        />
       </head>
       <body className={inter.className}>
         {/* <SessionProvider>  Wrap everything inside SessionProvider */}
