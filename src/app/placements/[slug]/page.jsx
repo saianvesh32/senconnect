a796e42a6f335c885aa9b28833164c6da9b0366b
firstrep <serlaanvesh@gmail.com>
2025-06-13 17:06:24 +0530

    slug errorfix

--- src/app/placements/[slug]/page.jsx
+++ src/app/placements/[slug]/page.jsx
@@ -60,3 +60,14 @@
 // }
 
 // export default SinglePostPage
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>hello World</h1>
+    </div>
+  )
+}
+
+export default page
